10b9303b758e26c695893a32fee53d04086fc687
Jonathan Guarnieri <djou.guarnieri@hotmail.com>
2025-11-17 19:54:05 -0300

    highlight selected square in board clicks

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -6,24 +6,29 @@ export default class extends Controller {
   }
 
   click(event) {
-    const id = event.currentTarget.id
+    const el = event.currentTarget
 
     if (!this.from) {
-      this.from = id
+      const elHasPiece = !!el.querySelector(".piece")
+      if (elHasPiece) {
+        this.from = el.id
+        el.classList.add("selected-square")
+      }
       return
     }
 
     const from = this.from
-    const to = id
+    const to = el.id
     this.from = null
 
-    const token = document.querySelector('meta[name="csrf-token"]').content
+    const prev = document.getElementById(from)
+    if (prev) prev.classList.remove("selected-square")
 
     fetch("/moves", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "X-CSRF-Token": token,
+        "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]').content,
         "Accept": "text/vnd.turbo-stream.html"
       },
       body: JSON.stringify({ from, to })
