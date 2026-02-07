f346a5c897388673234608819bd02721b680c5b2
Jonathan Guarnieri <djou.guarnieri@hotmail.com>
2026-02-07 18:51:38 -0300

    create play_castling_plan for BotMoveCalculator

--- app/services/bot_move_calculator.rb
+++ app/services/bot_move_calculator.rb
@@ -3,10 +3,65 @@
 
 class BotMoveCalculator
   def initialize(fen)
-    @fen = fen # FEN example: "rnbq1bnr/2ppkp1p/1p4p1/p7/4N3/3PB1N1/PPPQPPPP/2KR1B1R b - - 1 10"
+    @fen = fen
   end
 
   def call!
-    raise "No implemented yet"
+    calculate_move
+  end
+
+  private
+
+  def calculate_move
+    play_castling_plan
+  end
+
+  def play_castling_plan
+    if active_player == :black
+      return "e7", "e6" unless piece_on("e6")
+      return "f8", "e7" unless piece_on("e7")
+      return "g8", "f6" unless piece_on("f6")
+      return "e8", "g8" unless piece_on("g8")
+    end
+    # TODO: implement as white
+  end
+
+  def piece_on(square)
+    square_col = square.first
+    square_row = square.last
+
+    fen_by_row = @fen.split(' ').first.split('/')
+    pieces_by_row = fen_by_row.map do |fen_row|
+      row = []
+      fen_row.split('').each do |char|
+        if (Integer(char) rescue false)
+          row << Array.new(char.to_i)
+        else
+          row << char
+        end
+      end
+      row.flatten
+    end
+
+    cols = %w(a b c d e f g h)
+    rows = %w(8 7 6 5 4 3 2 1)
+
+    square_col_index = cols.index(square_col)
+    square_row_index = rows.index(square_row)
+
+    pieces_by_row[square_row_index][square_col_index]
+  end
+
+  def active_player
+    fen_player_char = @fen.split(' ').second
+
+    case fen_player_char
+    when 'b'
+      :black
+    when 'w'
+      :white
+    else
+      raise "Unable to define active_player"
+    end
   end
 end
